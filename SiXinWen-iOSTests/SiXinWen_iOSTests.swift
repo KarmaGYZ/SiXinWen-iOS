e772596a08c78ebc79935129eb8a1f17f82caf0e
KarmaGYZ <gyz94@sina.com>
2015-03-19 13:12:14 +0800

    Set up the project

---
+++ SiXinWen-iOSTests/SiXinWen_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SiXinWen_iOSTests.swift
+//  SiXinWen-iOSTests
+//
+//  Created by Karma Guo on 3/19/15.
+//  Copyright (c) 2015 SiXinWen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SiXinWen_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
